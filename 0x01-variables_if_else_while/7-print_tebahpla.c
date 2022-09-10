3c67ad80621ca71d424c2fd6e92f4dcedf2d0b28
tuneday-hub <eltswish@gmail.com>
2022-09-09 21:26:03 -0700

    Write a program that prints the lowercase al
    phabet in reverse, followed by a new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char w;
+
+	for (w = 'z';  w >= 'a'; w--)
+		putchar(w);
+	putchar('\n');
+	return (0);
+}
