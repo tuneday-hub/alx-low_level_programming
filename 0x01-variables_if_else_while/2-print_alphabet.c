250d21f7e33dfb99617c5e0f0fe3d725632506ef
tuneday-hub <eltswish@gmail.com>
2022-09-09 20:04:06 -0700

    Write a program that prints the alphabet in
    lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * 
+ */
+int main(void)
+{
+	char w;
+
+	for (w = 'a'; w <= 'z'; w++)
+		putchar(w);
+	putchar('\n');
+	return (0);
+}
