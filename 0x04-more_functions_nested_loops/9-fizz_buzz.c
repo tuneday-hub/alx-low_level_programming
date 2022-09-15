7a377486c0e4e33d796047eb05acb25dc59f9102
tuneday-hub <eltswish@gmail.com>
2022-09-15 16:44:22 -0700

    The FizzBuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/**
+ * main - prints fizz buzz
+ *
+ * author: tuneday
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0 && i / 20 != 5)
+		{
+			printf("Buzz ");
+		}
+		else if (i / 20 == 5)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+}
