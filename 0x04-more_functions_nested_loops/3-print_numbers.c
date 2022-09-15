c38694e0fa5b4df97367f378c03426517edb4526
tuneday-hub <eltswish@gmail.com>
2022-09-15 12:53:19 -0700

    Write a function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * author: tuneday
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
