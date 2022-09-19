c8c23ca0b10e651b654ecdfbb11c91eecde381cd
tuneday-hub <eltswish@gmail.com>
2022-09-19 14:06:06 -0700

    swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first parameter
+ * @b: second parameter
+ * author: tuneday
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = *c;
+}
