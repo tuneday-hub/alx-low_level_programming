fdc20e7298575290e4e41e156d37a773f90ba669
tuneday-hub <eltswish@gmail.com>
2022-09-19 16:03:20 -0700

    a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: parameter
+ * author: tuneday
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	j = i - 1;
+
+	while (j >= 0)
+	{
+		j--;
+	}
+}
