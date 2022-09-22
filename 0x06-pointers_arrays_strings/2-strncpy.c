32faa87acd0d49a117cc3e9010c700bc96e70c19
tuneday-hub <eltswish@gmail.com>
2022-09-22 08:57:55 -0700

    a function that c
    opies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: first string parameter
+ * @src: second string parameter
+ * @n: value
+ * Author: tuneday
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (src[i] != '\0')
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
