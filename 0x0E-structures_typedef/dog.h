0f4d525a2d727a63ed9b1cb90fd317235abda7a7
tuneday-hub <eltswish@gmail.com>
2022-10-16 02:22:01 -0700

    Define a new type struct
     dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_HEADER_FILE
 #define DOG_HEADER_FILE
+/**
+ * struct dog - user defined data type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * author: tuneday
+ */
 
 struct dog
 {
-	char *name;
+	char name[6];
 	float age;
-	char *owner;
+	char owner[4];
 }
 
 #endif
