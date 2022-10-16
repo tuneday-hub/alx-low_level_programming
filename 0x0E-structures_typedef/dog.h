b291a973e6f69bc4f85b45ff9c2ce5b3e1dd78d1
tuneday-hub <eltswish@gmail.com>
2022-10-16 16:21:34 -0700

    Define a new type struct
     dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,6 @@
  * @owner: owner of the dog
  * author: tuneday
  */
-
 struct dog
 {
 	char name[6];
@@ -19,5 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
