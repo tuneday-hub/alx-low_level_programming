977dd07b0316f8c2837909d076212c57048fef60
root <root@DESKTOP-R7QACGC.localdomain>
2022-10-26 07:27:17 +0100

    My first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_array - creates an array of chars dynamically
+ * @size: size of the array
+ * @c: string
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	buffer = (char *) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+		while (position < size)
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+		return (buffer);
+	}
+}
