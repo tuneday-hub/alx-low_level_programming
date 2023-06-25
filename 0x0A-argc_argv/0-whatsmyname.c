882f1094c6485f36cff47decdc8ecf4c56c39268
tuneday-hub <eltswish@gmail.com>
2023-06-25 18:18:39 +0100

    Completed Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * @argv: Arguments
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(unsigned int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
