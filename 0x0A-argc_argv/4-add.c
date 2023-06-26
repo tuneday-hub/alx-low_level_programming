81e2737f1759e88108ed8421f6ca7217b6d7cadd
tuneday-hub <eltswish@gmail.com>
2023-06-26 09:41:07 +0100

    Completed Task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints the name of the program
+ * @argc: Number of arguments
+ * @argv: Arguments
+ * Return: 0 if successful else 1 then print "Error"
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	if (argc == 1)
+	{
+		sum = 0;
+		printf("%i\n", sum);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(argv[i]))
+			{
+				sum = "";
+				printf("Error\n");
+
+				return (1);
+
+				break;
+			}
+			else
+			{ 
+				sum += atoi(argv[i]);
+			}
+			printf("%i\n", sum);
+		}
+	}
+
+	return (0);
+}
