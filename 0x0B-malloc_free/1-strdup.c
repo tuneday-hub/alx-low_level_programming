e78d1e1fa64e8c74cd4a88f559950ec3312f90ba
tuneday-hub <eltswish@gmail.com>
2023-07-01 15:06:52 +0100

    Completed Task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strdup - array for prints a string
+ * @str: array of elements
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *rtnPtr;
+	int lenOfStr;
+	int count;
+
+	lenOfStr = 0;
+	while (str[lenOfStr != '\0'])
+	{
+		lenOfStr++;
+	}
+
+	rtnPtr = (char *) malloc((lenOfStr * sizeof(char)) + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	if (rtnPtr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (count = 0; count < (lenOfStr + 1); count++)
+		{
+			rtnPtr[count] = str[count];
+		}
+		return (rtnPtr);
+	}
+}
