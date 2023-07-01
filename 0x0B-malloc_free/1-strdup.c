4e592bf6bd783a4e923729dbf50ccd76fc8f2fc1
tuneday-hub <eltswish@gmail.com>
2023-07-01 16:44:43 +0100

    Completed Task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,39 +2,64 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * _strlen - count array
+ * @s: array of elements
+ * Return: an int
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strcpy - copy arrays
+ * @src: array of elements
+ * @dest: dest array
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
  * _strdup - array for prints a string
  * @str: array of elements
  * Return: pointer
  */
 char *_strdup(char *str)
 {
-	char *rtnPtr;
-	int lenOfStr;
-	int count;
+	char *dst;
 
-	lenOfStr = 0;
-	while (str[lenOfStr != '\0'])
-	{
-		lenOfStr++;
-	}
-
-	rtnPtr = (char *) malloc((lenOfStr * sizeof(char)) + 1);
-
-	if (str == 0)
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	
-	if (rtnPtr == 0)
+
+	dst = (char *) malloc((_strlen(str) * sizeof(char)) + 1);
+
+	if (dst == NULL)
 	{
 		return (NULL);
 	}
-	else
-	{
-		for (count = 0; count < lenOfStr; count++)
-		{
-			rtnPtr[count] = str[count];
-		}
-		return (rtnPtr);
-	}
+
+	_strcpy(dst, str);
+	return (dst);
 }
