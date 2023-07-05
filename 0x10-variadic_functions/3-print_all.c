0adbed8511c890ad3b210ea285c63a20a75600da
tuneday-hub <eltswish@gmail.com>
2023-07-05 09:41:00 +0100

    Completed Task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,17 +11,18 @@ void print_all(const char * const format, ...)
 	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
+
 	while (format && format[i])
 	{
 		j = 0;
+
 		while (t_arg[j])
 		{
 			if (format[i] == t_arg[j] && k)
 			{
 				printf(", ");
 				break;
-			}
-			j++;
+			} j++;
 		}
 		switch (format[i])
 		{
@@ -43,8 +44,7 @@ void print_all(const char * const format, ...)
 				}
 				printf("%s", str);
 				break;
-		}
-		i++;
+		} i++;
 	}
 	printf("\n"), va_end(valist);
 }
